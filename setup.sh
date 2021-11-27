dcbcd10bfed14bfc560232b50648957d3bfc710c
Vlad Usatii <vladusatii@gmail.com>
2021-11-26 19:43:34 -0600

    working patch

--- setup.sh
+++ setup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-# To install, run this setup shell script
-# NEW:
-# > chmod +x setup.py
-# > ./setup.py
-# OLD:
-# > ./main.py [ COMMAND ]
 
 pip install -r requirements.txt
 chmod +x main.py
-python3 alias.py
+echo "alias cls='python3 main.py'" >> ~/.bash_profile
